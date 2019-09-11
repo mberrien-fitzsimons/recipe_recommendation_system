040e614b0d159acf2844ef8af0afcb9221d3b7c2
mishaberrien <markishab@gmail.com>
2019-09-11 15:04:39 -0400

    made function to scrape marianos site

--- src/d01_data/web_scraping.py
+++ src/d01_data/web_scraping.py
@@ -98,3 +98,71 @@ def sr_scraping():
         exec("df{}.to_csv('../../data/01_raw/simply_recipes/simply_recipes_{}.csv')".format(count, count))
 
         count = count + 1
+
+def marianos_insta_scraping():
+    # dictionary of Marianos Aile Numbers on Instacart
+
+    url = "https://www.instacart.com/"
+    browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+    browser.get(url) #navigate to the page
+
+    time.sleep(70)
+
+    instacart_ailes = {'produce': [1370, 1365, 1367, 1368, 1373, 1366],
+        'frozen': [1473, 1472, 1478, 1476, 4302, 1474, 1489, 2997, 1475, 3799, 1477],
+         'bakery': [1443, 1442, 1479, 1441, 1444],
+         'alcohol': [1445, 1446, 1450, 1449, 1448, 1447, 2708],
+         'international': [1408, 1409, 1483, 1410, 1411],
+         'beverages': [1455, 1453, 1451, 1452, 1456, 1491, 1454, 2780],
+         'pets': [1378, 1379, 1380],
+         'dry goods pasta': [1429, 3798, 1482, 1431, 1430, 2745, 3600, 2746],
+         'bulk': [3657, 3662, 3664, 6374, 6378, 3658, 3661, 3663, 3659, 6377, 3665],
+         'personal care': [3793, 1384, 2720, 1395, 1392, 3794, 3795, 1387, 3796, 1388, 3651, 6458, 1390, 3792, 1393, 1382, 1383, 3412, 3011, 3012, 6492],
+         'meat seafood': [1464, 1470, 1465, 1488, 2768, 1487, 1471],
+         'pantry': [1419, 1420, 1428, 1421, 1426, 1423, 1422, 3747, 1496, 1496, 1424, 1425, 3605],
+         'breakfast': [1413, 1481, 1480, 1412],
+         'canned goods': [1414, 1415, 1416, 1417, 1418],
+         'dairy eggs': [1459, 1462, 1461, 1457, 1460, 1458, 4251, 1463, 1498],
+         'household': [1402, 1403, 1398, 1405, 1404, 1399, 1400, 1401, 1407, 1406, 6493],
+         'babies': [1437, 1438, 1440, 1439, 3642],
+         'snacks': [1435, 1432, 1433, 1434, 1497, 1485, 1486, 1436, 2709, 3014, 1484],
+         'deli': [1375, 1374, 1494, 3603, 1377, 6664]}
+
+    # Convert dictionary to list of aile numbers
+    aile_numbers = []
+    for list in instacart_ailes.values():
+        for val in list:
+            aile_numbers.append(val)
+    aile_numbers.sort()
+
+    # Now let's start scraping the website
+    aile_numbers
+    total_num_ailes = len(aile_numbers)
+    count = 1
+
+    for aile in aile_numbers[62:]:
+
+        browser.get("https://www.instacart.com/store/marianos/departments/199/aisles/{}".format(aile))
+
+        time.sleep(3)
+
+        for i in range(0,40):
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+
+        sel = Selector(text = browser.page_source)
+
+        time.sleep(5)
+        count = count + 1
+
+
+        product = sel.xpath("//span[@class='full-item-name']/text()").extract()
+        unit_price = sel.xpath('//div[@class="item-price"]/span/span[1]/text()').extract()
+    #     item_size = sel.xpath("//span[@class='item-size muted']//text()").extract()
+        item_size = sel.xpath('//*[@class="item-card"]').extract()
+        product_aile = sel.xpath("//h1[@data-radium]//text()").extract()
+        prod_aile_count = (product_aile * len(product))
+
+        exec("df{} = pd.DataFrame(zip(product, unit_price, item_size, prod_aile_count), columns =['product', 'unit_price', 'item_size', 'prod_aile'])".format(aile))
+
+        exec("df{}.to_csv('../../data/01_raw/prod_aile_{}.csv', index=False)".format(aile, aile))
