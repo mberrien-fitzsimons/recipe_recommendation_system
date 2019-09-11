028208d6910b24a618f397104f4761b7af9cbc27
mishaberrien <markishab@gmail.com>
2019-09-11 18:27:05 -0400

    built initial simply recipes model

--- src/d00_utils/utils.py
+++ src/d00_utils/utils.py
@@ -19,3 +19,90 @@ def read_multiple_csv_and_concat(file_location_pattern_and_name_pattern):
     frame = pd.concat(li, axis=0, ignore_index=True)
 
     return frame
+
+def singularize(word):
+    """
+    A poor replacement for the pattern.en singularize function, but ok for now.
+    """
+
+    units = {
+        "cups": u"cup",
+        "tablespoons": u"tablespoon",
+        "teaspoons": u"teaspoon",
+        "pounds": u"pound",
+        "ounces": u"ounce",
+        "cloves": u"clove",
+        "sprigs": u"sprig",
+        "pinches": u"pinch",
+        "bunches": u"bunch",
+        "slices": u"slice",
+        "grams": u"gram",
+        "heads": u"head",
+        "quarts": u"quart",
+        "stalks": u"stalk",
+        "pints": u"pint",
+        "pieces": u"piece",
+        "sticks": u"stick",
+        "dashes": u"dash",
+        "fillets": u"fillet",
+        "cans": u"can",
+        "ears": u"ear",
+        "packages": u"package",
+        "strips": u"strip",
+        "bulbs": u"bulb",
+        "bottles": u"bottle"
+    }
+
+    if word in units.keys():
+        return units[word]
+    else:
+        return word
+
+def word2features(doc, i):
+    word = doc[i][0]
+    postag = doc[i][1]
+
+    # Common features for all words
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'postag=' + postag
+    ]
+
+    # Features for words that are not
+    # at the beginning of a document
+    if i > 0:
+        word1 = doc[i-1][0]
+        postag1 = doc[i-1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'beginning of a document'
+        features.append('BOS')
+
+    # Features for words that are not
+    # at the end of a document
+    if i < len(doc)-1:
+        word1 = doc[i+1][0]
+        postag1 = doc[i+1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'end of a document'
+        features.append('EOS')
+
+    return features
+
+# A function for extracting features in documents
+def extract_features(doc):
+    return [word2features(doc, i) for i in range(len(doc))]
+
+# A function fo generating the list of labels for each document
+def get_labels(doc):
+    return [label for (token, postag, label) in doc]
---
+++ src/d02_features/feature_creation.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import argparse
+import nltk
+import pandas as pd
+import pycrfsuite
+import numpy as np
+import re
+import os
+import sys
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import MultiLabelBinarizer
+
+from d00_utils.utils import singularize, word2features, extract_features, get_labels
+
+src_dir = os.path.join(os.getcwd(), '..')
+sys.path.append(src_dir)
+
+def nyt_ingredients_crf_feature_creation(nyt_ing):
+
+    nyt_ing_tuple = []
+    for index, row in nyt_ing.iterrows():
+        name = row[1]
+        qty = row[2]
+        unit = row[4]
+        comment = row[5]
+        nyt_ing_tuple.append([(qty, 'qty'), (unit, 'unit'),
+        (name, 'name'),
+        (comment, 'comment')])
+    # break up tuples with multiple words
+    nyt_ing_tuple_new = []
+    for sub_list in nyt_ing_tuple:
+        sub_ls = []
+        for elem in sub_list:
+            if " " in str(elem[0]):
+                elem2 = elem[0].split(" ")
+                for el in elem2:
+                    sub_ls.append((el, elem[1]))
+            else:
+                sub_ls.append((elem[0], elem[1]))
+        nyt_ing_tuple_new.append(sub_ls)
+    # erase tuples with missing values
+    for idx_big, ingredient in enumerate(nyt_ing_tuple_new):
+        for idx, word in enumerate(ingredient):
+            if word[0] == 'missing':
+                nyt_ing_tuple_new[idx_big].remove(word)
+            if word[0] == '':
+                nyt_ing_tuple_new[idx_big].remove(word)
+
+    # strip all puncctuation from tuple
+    crf_data = []
+    for sub_list in nyt_ing_tuple_new:
+        sublist = []
+        for word in sub_list:
+            word2 = str(word[0]).strip(')!,.?(')
+            sublist.append((word2, word[1]))
+        crf_data.append(sublist)
+
+    count = 1
+    while count <= 2:
+        count = count + 1
+        for idx_big, ingredient in enumerate(crf_data):
+            for idx, word in enumerate(ingredient):
+                if word[0] == 'missing':
+                    crf_data[idx_big].remove(word)
+                if word[0] == '':
+                    crf_data[idx_big].remove(word)
+     # add parts of speech
+    data_nyt = []
+    for i, doc in enumerate(crf_data):
+
+        # Obtain the list of tokens in the document
+        tokens = [t for t, label in doc]
+
+        # Perform POS tagging
+        try:
+            tagged = nltk.pos_tag(tokens)
+        except:
+            tagged = 'missing'
+
+        # Take the word, POS tag, and its label
+        data_nyt.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
+
+    X = [extract_features(doc) for doc in data_nyt]
+    y = [get_labels(doc) for doc in data_nyt]
+
+    return [X, y]
