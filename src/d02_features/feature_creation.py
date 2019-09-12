05955d08bfa2b5bbfd9299bfea4e669ec825b624
mishaberrien <markishab@gmail.com>
2019-09-11 20:54:26 -0400

    got app to run in a jupyter notebook

--- src/d02_features/feature_creation.py
+++ src/d02_features/feature_creation.py
@@ -86,3 +86,79 @@ def nyt_ingredients_crf_feature_creation(nyt_ing):
     y = [get_labels(doc) for doc in data_nyt]
 
     return [X, y]
+
+from d00_utils.utils import singularize, word2features, extract_features, get_labels
+
+nltk.download('averaged_perceptron_tagger')
+
+def instacart_prod_crf_feature_creation(instacart_prod_train):
+
+    instacart_prod_train.fillna("missing", inplace=True)
+    insta_product_tuple = []
+
+    for index, row in instacart_prod_train.iterrows():
+        pre_description = row[1].lower()
+        food = row[2].lower()
+        post_description = row[3].lower()
+        insta_product_tuple.append([(pre_description, 'pre'), (food, 'food'), (post_description, 'post')])
+
+    # break up the tuple into multiple WORDS
+    insta_product_tuple_new = []
+    for sub_list in insta_product_tuple:
+        sub_ls = []
+        for elem in sub_list:
+            if " " in str(elem[0]):
+                elem2 = elem[0].split(" ")
+                for el in elem2:
+                    sub_ls.append((el, elem[1]))
+            else:
+                sub_ls.append((elem[0], elem[1]))
+        insta_product_tuple_new.append(sub_ls)
+
+    # erase tuples with missing values
+    for idx_big, product in enumerate(insta_product_tuple_new):
+        for idx, word in enumerate(product):
+            if word[0] == 'missing':
+                insta_product_tuple_new[idx_big].remove(word)
+            if word[0] == '':
+                insta_product_tuple_new[idx_big].remove(word)
+
+    # strip puncctuation
+    crf_data = []
+    for sub_list in insta_product_tuple_new:
+        sublist = []
+        for word in sub_list:
+            word2 = str(word[0]).strip(')!,.?(')
+            sublist.append((word2, word[1]))
+        crf_data.append(sublist)
+
+    count = 1
+    while count <= 2:
+        count = count + 1
+        for idx_big, ingredient in enumerate(crf_data):
+            for idx, word in enumerate(ingredient):
+                if word[0] == 'missing':
+                    crf_data[idx_big].remove(word)
+                if word[0] == '':
+                    crf_data[idx_big].remove(word)
+
+    # construct Features
+    data_instacart = []
+    for i, doc in enumerate(crf_data):
+
+        # Obtain the list of tokens in the document
+        tokens = [t for t, label in doc]
+
+        # Perform POS tagging
+        try:
+            tagged = nltk.pos_tag(tokens)
+        except:
+            tagged = 'missing'
+
+        # Take the word, POS tag, and its label
+        data_instacart.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
+
+    X = [extract_features(doc) for doc in data_instacart]
+    y = [get_labels(doc) for doc in data_instacart]
+
+    return [X, y]
--- src/d03_models/crf_model_baskets.py
+++ src/d03_models/crf_model_baskets.py
@@ -14,9 +14,93 @@ from sklearn.metrics import classification_report
 from sklearn.preprocessing import MultiLabelBinarizer
 from nltk.tokenize import sent_tokenize, word_tokenize, RegexpTokenizer
 
+nltk.download('averaged_perceptron_tagger')
+
 src_dir = os.path.join(os.getcwd(), '..')
 sys.path.append(src_dir)
 
 from d00_utils.utils import singularize, word2features, extract_features, get_labels
 
-nltk.download('averaged_perceptron_tagger')
+def product_tagger(product_sentence_tokens, product_label_sentence):
+    pre = []
+    food = []
+    post = []
+
+    for word, label in zip(product_sentence_tokens, product_label_sentence):
+        if label == 'pre':
+            pre.append(str(word).lower())
+        if label == 'food':
+            food.append(word)
+        if label == 'post':
+            post.append(str(word).lower())
+    return {'pre': " ".join(pre), 'food': " ".join(food), 'post': " ".join(post)}
+
+def token_labels_to_dict(tokens, labels, titles):
+    final_dict = {}
+    for token, label, title in zip(tokens, labels, titles):
+        prod = product_tagger(token, label)
+        final_dict[str(title).lower()] = prod
+        # final_dict[str(title)] = prod
+    return final_dict
+def food_label_finder(token_list, label_list):
+    pre = []
+    food = []
+    post = []
+
+    for word, label in zip(token_list, label_list):
+        if label == 'pre':
+            pre.append(word.lower())
+        if label == 'food':
+            food.append(word.lower())
+        if label == 'post':
+            post.append(word.lower())
+    return " ".join(food)
+
+# A function for extracting features in documents
+def extract_features(doc):
+    return [word2features(doc, i) for i in range(len(doc))]
+
+def crf_model_basket_tagger(instacart_baskets):
+    instacart_baskets.drop(columns = ['add_to_cart_order', 'reordered', 'aisle_id',
+                        'department_id', 'aisle', 'eval_set', 'order_number',
+                        'order_dow', 'order_hour_of_day'], inplace=True)
+
+    instacart_baskets_food = instacart_baskets.loc[(instacart_baskets['department']!='personal care')&
+                                               (instacart_baskets['department']!='household')&
+                                               (instacart_baskets['department']!='babies')&
+                                               (instacart_baskets['department']!='pets')&
+                                               (instacart_baskets['department']!='other')&
+                                               (instacart_baskets['department']!='alcohol')&
+                                               (instacart_baskets['department']!='missing')&
+                                               (instacart_baskets['department']!='beverages')&
+                                               (instacart_baskets['department']!='snacks')]
+
+    products_list = list(instacart_baskets_food.product_name)
+
+    # let's tokenize all the words and get rid of punctuation
+    tokenizer = RegexpTokenizer(r'(\d\/\d |\w+)')
+    token_sr = []
+    for product in products_list:
+        token_sr.append(tokenizer.tokenize(product))
+
+    crf_data  = []
+    for product in token_sr:
+        pos = nltk.pos_tag(product)
+        crf_data.append(pos)
+
+    X = [extract_features(doc) for doc in crf_data]
+
+    tagger = pycrfsuite.Tagger()
+    tagger.open('../../data/04_models/crf_instacart_products_final.model')
+
+    y_pred = [tagger.tag(xseq) for xseq in X]
+
+    final_dict = token_labels_to_dict(token_sr, y_pred, products_list)
+
+    prod_list_new = []
+    for token, label in zip(token_sr, y_pred):
+        prod_list_new.append(food_label_finder(token, label))
+
+    instacart_baskets_food['new_prod_list'] = prod_list_new
+
+    return instacart_baskets_food
